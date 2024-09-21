23c6bf2c4348a05788e10ea2d7fbb98624fb6338
Omar <omar.tourabi7@gmail.com>
2024-09-21 18:18:16 +0200

    ADD: now can get and delete user by id

--- controllers/users.go
+++ controllers/users.go
@@ -8,28 +8,15 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Users(ctx *gin.RouterGroup) {
     ctx.GET("", GetAllUsers)
     ctx.POST("", CreateUser)
 
-    ctx.GET("/:userId", GetUser)
-}
-
-func FetchUsers() ([]models.User, error) {
-    file, err := os.Open("users.json")
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
-
-    var users []models.User
-
-    if err = json.NewDecoder(file).Decode(&users); err != nil {
-        return nil, err
-    }
-    return users, nil
+    ctx.GET("/:userId", GetUserById)
+    ctx.DELETE("/:userId", DeleteUser)
 }
 
 func GetAllUsers(ctx *gin.Context) {
@@ -51,29 +38,52 @@ func CreateUser(ctx *gin.Context) {
         return
     }
 
+    hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), 10)
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
     user := models.CreateUserDto{
         Nickname: input.Nickname,
         Email: input.Email,
-        Password: input.Password,
+        Password: string(hashedPassword),
     }
-
-    // - Ajouter en base de donnee si les donnees sont OK (email et nickname unique)
-
-    ctx.JSON(http.StatusOK, user)
-}
-
-func GetUser(ctx *gin.Context) {
     users, err := FetchUsers()
     if err != nil {
         ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
 
-    idStr := ctx.Params.ByName("userId")
+    var idx uint = 1
+    if len(users) > 0 {
+        idx = users[len(users)-1].ID + 1
+    }
 
-    id, err := strconv.Atoi(idStr)
+    newUser := models.User{
+        ID: idx,
+        Nickname: user.Nickname,
+        Email: user.Email,
+        Password: user.Password,
+    }
+
+    users = append(users, newUser)
+    if err := UpdateFile("users.json", users); err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    ctx.JSON(http.StatusOK, newUser)
+}
+
+func GetUserById(ctx *gin.Context) {
+    id, err := GetUserId(ctx.Params)
     if err != nil {
-        ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid userId"})
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    users, err := FetchUsers()
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
         return
     }
 
@@ -86,4 +96,81 @@ func GetUser(ctx *gin.Context) {
     ctx.String(http.StatusBadRequest, "User not found")
 }
 
+
+func DeleteUser(ctx *gin.Context) {
+    id, err := GetUserId(ctx.Params)
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+
+    users, err := FetchUsers()
+    if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    var target *models.User
+    var indexToRemove int
+    for i, user := range users {
+        if user.ID == uint(id) {
+            target = &user
+            indexToRemove = i
+            break
+        }
+    }
+
+    if (target == nil) {
+        ctx.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+        return
+    }
+
+    users = append(users[:indexToRemove], users[indexToRemove + 1:]...)
+    if err := UpdateFile("users.json", users); err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    ctx.String(http.StatusOK, "User deleted")
+}
+
+func FetchUsers() ([]models.User, error) {
+    file, err := os.Open("users.json")
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var users []models.User
+
+    if err = json.NewDecoder(file).Decode(&users); err != nil {
+        return nil, err
+    }
+    return users, nil
+}
+
+func UpdateFile(name string, data interface{}) error {
+    fileUpdated, err := os.OpenFile(name, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+    if err != nil {
+        return err
+    }
+    defer fileUpdated.Close()
+    bytes, err := json.Marshal(data)
+    if err != nil {
+        return err
+    }
+    _, err = fileUpdated.Write(bytes)
+    if err != nil {
+        return err
+    }
+    return nil
+}
+
+func GetUserId(params gin.Params) (int, error) {
+    idStr := params.ByName("userId")
+
+    id, err := strconv.Atoi(idStr)
+    if err != nil {
+        return 0, err
+    }
+    return id, nil
+}
 
--- models/user.go
+++ models/user.go
@@ -1,14 +1,13 @@
 package models
 
-import "gorm.io/gorm"
+// import "gorm.io/gorm"
 
 type User struct {
-    gorm.Model
+    // gorm.Model
     ID uint `json:"id" gorm:"primary_key"` 
     Nickname string `json:"nickname" gorm:"unique;not null" binding:"required,min=3"`
     Email string `json:"email" gorm:"unique;not null" binding:"required,email"`
     Password string `json:"password" gorm:"not null" binding:"required,min=6"`
-    // A completer au fur et a mesure
 }
 
 type CreateUserDto struct {
